7269d4cbe74f982f8d2f64afb6e2cac7bf960f62
Jonsson <vjonsson@ppxhpcacc01.coh.org>
2020-08-10 00:53:35 -0700

    wrote large part of pipeline

--- src/ab_pipeline/antibody_pipeline_vdj.py
+++ src/ab_pipeline/antibody_pipeline_vdj.py
@@ -7,20 +7,21 @@ data_path = '../../data/'
 estimator_path = data_path + 'estimator/'
 location_path = data_path + 'location/'
 pdb_path = data_path + 'pdb/'
+out_path = data_path + 'ddg/' 
 
 foldx_path = '/Applications/foldx5MacC11/'
-
+foldx_path = ''
 
 aa_three = ['ALA','ARG','ASN','ASP','CYS','GLU','GLN','GLY','HIS','ILE', 'LEU', 'LYS', 'MET','PHE', 'PRO','SER', 'THR','TRP', 'TYR','VAL']
 aa_single = ['A','R','N','D','C','E','Q','G','H','I','L','K','M','F', 'P', 'S', 'T','W', 'Y','V']
 aa_1to3_dic = dict(zip(aa_single, aa_three))
 aa_3to1_dic = dict(zip(aa_three, aa_single))
 
-def read_ddg_file(pdb_name, header=None):
+def read_ddg_file(ab_name,pdb_name, header=None):
 
     prefix='PS_'
     suffix ='_scanning_output.txt'
-    df = pd.read_table('../../data/ddg/' + prefix + pdb_name +suffix,header=header)
+    df = pd.read_table(out_path +ab_name +'/'+ prefix + pdb_name +suffix,header=header)
     df = df.rename(columns= {0:'mut', 1:'dg'})
     return df
 
@@ -33,14 +34,15 @@ def read_estimator(filename, ab):
         retval = False
     return retval, dfss 
 
-def calculate_ddg_bind(p1, p2, ddg_name):
+def calculate_ddg_bind(ab_name,p, p_less):
 
-    dg1 = read_ddg_file(pdb_name=p1,header=None)
-    dg2 = read_ddg_file(pdb_name=p2, header=None)
+    dg1 = read_ddg_file(ab_name,pdb_name=p,header=None)
+    dg2 = read_ddg_file(ab_name, pdb_name=p_less, header=None)
     ddg = pd.merge(dg1, dg2, on='mut')
+    ddg_name = 'ddg'
     ddg[ddg_name] = ddg['dg_x'] -ddg['dg_y']
-    ddg.to_csv('./ddG_' + p1 +'_' + p2 +'.csv')
-    return ddg[['mut', ddg_name]]
+    ddg.to_csv('./ddG_' + p +'_' + p_less +'.csv')
+    return ddg[['mut', 'ddg']]
 
 def construct_positionscan_string (pdb_name, ab_pos, pdb_loc):
 
@@ -63,11 +65,17 @@ def get_mutation_positions(ab):
 
     # get positions where wildtype== True and coeff>=0 
     # constrain also after reordering 
-    mut1 = ab.loc[(ab.wild_type == True) & (ab.coefficient >=1e-10) & (ab.chain == 'H') & (ab.coefficient.nlargest(n=200))]
-    mut2 = ab.loc[(ab.wild_type == False) & (ab.coefficient <= -1e-10) & (ab.chain =='H') & (ab.coefficient.nsmallest(n=200))]
+
+    mut1 = ab.loc[(ab.wild_type == True) & (ab.coefficient.astype(float) >=1e-10) & (ab.chain == 'H')]
+    mut1 = mut1.loc[mut1.coefficient.astype(float).nlargest(n=10).index]
+
+    mut2 = ab.loc[(ab.wild_type == False) & (ab.coefficient <= -1e-10) & (ab.chain =='H')]
+    mut2 = mut2.loc[mut2.coefficient.astype(float).nsmallest(n=10).index]
 
     # focus on the heavy chain
     abs = pd.concat([mut1, mut2])
+
+    print (abs)
     return abs 
     
 def read_pdb_locations(ab_name):
@@ -80,7 +88,8 @@ def read_pdb_locations(ab_name):
 def run_position_scan (ab_name, pdb_name, pos_scan):
 
     ab_path = pdb_path + ab_name + '/'
-    out_path = data_path + 'ddg/' + ab_name + '/'
+    out_path = out_path + ab_name + '/'
+
     print('Running position scan')
     print(ab_path)
     print (pos_scan)
@@ -91,22 +100,43 @@ def run_position_scan (ab_name, pdb_name, pos_scan):
 
 def run_build_model(ab_name, pdb_name, mutations_file): 
 
+    ab_path = pdb_path + ab_name + '/'
+    out_path = pdb_path + ab_name + '/'
     print(pdb_name)
-    print(mutations_file)
-    command = "foldx --command=BuildModel --pdb="+pdb_name+".pdb "
-    command  = command + "--mutant-file=" + mutations_file
-    print (command)
+    command = foldx_path + "foldx --command=BuildModel --pdb-dir=" + ab_path + " --pdb=" + pdb_name
+    command = command + " --mutant-file="+ mutations_file + " --output-dir=" + out_path
+    print(command)
     os.system(command)
+
+def rename_pdb_files(ab_name, pdb_name, mutations):
+
+    ab_path = pdb_path + ab_name + '/'
+    out_path = pdb_path + ab_name + '/'
+    print(pdb_name)
+    i = 1 
+    for mut in mutations:
+        pdb =  out_path + pdb_name
+        command  = 'mv ' + pdb + '_' + str(i) + '.pdb ' + pdb + '_' + mut + '.pdb'
+        print(command)
+        os.system(command)
+        i = i+1
+
 
 def run_repair_model(ab_name, pdb_name):
+
+    ab_path = pdb_path + ab_name + '/'
+    out_path = pdb_path + ab_name + '/'
     print(pdb_name)
-    command = "foldx --command=RepairPDB --pdb="+pdb_name+".pdb "
+    command = foldx_path + "foldx --command=RepairPDB --pdb-dir=" + ab_path + " --pdb=" + pdb_name
+    command = command +  " --output-dir=" + out_path
+    print(command)
     os.system(command)
 
-def run_multiple_repair_model(pdb_name, mutations):
+def run_multiple_repair_model(ab_name, pdb_name, mutations):
     i = 1
-    for mut in muts:
-        run_repair_model(pdb_name, pdb_name + '_' + mut)
+    for mut in mutations:
+        print(mut)
+        run_repair_model(ab_name, pdb_name + '_' + mut +'.pdb')
         i = i+1
 
 def calculate_ddgs_from_estimator_coefficients(file_estimator, ab_name,pdb_struct):
@@ -119,17 +149,29 @@ def calculate_ddgs_from_estimator_coefficients(file_estimator, ab_name,pdb_struc
     pos_scan = construct_positionscan_string (pdb_name, ab_pos, pdb_loc)
     run_position_scan (ab_name, pdb_struct, pos_scan)
 
+def build_optimal_antibody_structure_mutations(ab_name, p, p_less, upper_bound_ddg = -0.4):
+    mutations = get_optimal_antibody_structure_mutations(ab_name, p, p_less, upper_bound_ddg = -0.4)
+    write_to_mutations_file(ab_name, mutations, 'individual_list.txt')
+    run_build_model(ab_name, p + '.pdb', 'individual_list.txt')
+    run_build_model(ab_name, p_less + '.pdb', 'individual_list.txt')
+    rename_pdb_files(ab_name, p, mutations)
+    rename_pdb_files(ab_name, p_less, mutations)
+    run_multiple_repair_model(ab_name,p, mutations)
+    run_multiple_repair_model(ab_name,p_less, mutations)
+    return mutations
 
-def get_optimal_antibody_structure_mutations(p1, p2, upper_bound_ddg = -0.4):
+def get_optimal_antibody_structure_mutations(ab_name, p, p_less, upper_bound_ddg = -0.4):
 
-    ddg = calculate_ddg_bind(p1, p2)
-    ddg_sort = ddg.sort_values(by=1).reset_index()
-    ddg_sort = ddg_sort.rename(columns = {0:'mut', 1:'ddg'})
+    ddg = calculate_ddg_bind(ab_name,p, p_less)
+    print (ddg)
+    ddg_sort = ddg.sort_values(by='ddg').reset_index()
+    #ddg_sort = ddg_sort.rename(columns = {0:'mut', 1:'ddg'})
 
     ddg_less = ddg_sort.loc[ddg_sort.ddg < -0.4 ]
     ddg_less['loc'] = ddg_less['mut'].str[4:-1]
     grouped = ddg_less.groupby(by='loc').count().reset_index().sort_values('ddg', ascending =False).reset_index()
 
+    print (ddg_less)
     muts = []
     for location in grouped['loc'].values:
         mut= ddg_less.loc[ddg_less['loc'] == location].iloc[0].mut
@@ -140,14 +182,21 @@ def get_optimal_antibody_structure_mutations(p1, p2, upper_bound_ddg = -0.4):
     return muts
 
 
-def write_to_mutations_file(mutations, mutations_filename):
+def write_to_mutations_file(ab_name, mutations, mutations_filename):
+
+    ab_path = pdb_path + ab_name + '/'
+    out_path = pdb_path + ab_name + '/'
+
     mutstr = ''
-    for mut in muts:
+    for mut in mutations:
         mutstr = mutstr + mut+ ';\n'            
     f  = open(mutations_filename, 'w')   
     f.write(mutstr)
     f.close()
 
+    f  = open(ab_path + 'mutations.txt', 'w')
+    f.write(mutstr)
+    f.close()
 
 # TEA: docstring everything
 # TEA:  create a separate main function and file with command line arguments  
@@ -172,27 +221,27 @@ def write_to_mutations_file(mutations, mutations_filename):
 #ab_name = 'B38'
 #p1 = '7bz5_Repair'
 
-#ab_name = 'CB6'
-#p1 = '7c01_Repair'
+ab_name = 'CB6'
+p = '7c01_Repair'
 
 #ab_name = 'CV30'
 #p1 = '6xe1_Repair'
 #f = 'NeutSeqData_VH3-53_66_aligned_mapped_coefficients.csv'
 
+# doesnt' work i don't know why 
 #ab_name = 'CC12.1'
 #p1 = '6xc3_CC12.1_Repair'
 
 
-
 #calculate_ddgs_from_estimator_coefficients(f, ab_name = ab_name, pdb_struct=p1 +'.pdb')
-#calculate_ddgs_from_estimator_coefficients(f, ab_name = ab_name, pdb_struct=p1+'_less_virus_Repair.pdb')
-
 # Get the mutations needed to  optimize antibody binding, these are just estimator locations with coefficients < -0.4 
 
-#mutations = get_optimal_antibody_structure_mutations(p1, p2, upper_bound_ddg = -0.4)
-#mutations = get_optimal_antibody_structure_mutations(p1, p2, upper_bound_ddg = -0.4)
+less_str = '_less_virus_Repair'
 
-#write_to_mutations_file(mutations, 'individual_list.txt')
+#build_optimal_antibody_structure_mutations(ab_name,p, p+less_str, upper_bound_ddg = -0.4)
+
+#run_multiple_repair_model(pdb_name=p, mutations)
+
 
 # Generate mutated PDBs 
 # run_build_model('C105' + mut, p1, 'individual_list.txt')
@@ -200,8 +249,8 @@ def write_to_mutations_file(mutations, mutations_filename):
 # TEA: rename all the repaired files output from build model from xx_number to xx_mutation 
 # TEA: remove the antibody from these structures and save to _less_ab.pdb
 # TEA: Now repair all mutated structures keep track of these from mutations file 
-# run_multiple_repair_model(pdb_name=p1, mutations)
 
+#run_multiple_repair_model(pdb_name=p1, mutations)
 # TEA: For all the repaired structures, (here just as an example of two, but you will get them from the mutations file)
 # p1_m  = '6XCM_Repair_TH28D_Repair.pdb'
 # p2_m = '6XCM_Repair_TH28D_Repair_less_ab_Repair.pdb'
@@ -232,14 +281,13 @@ def write_to_mutations_file(mutations, mutations_filename):
 #run_position_scan (p1_wt, p1_wt, posscan_str)
 #run_position_scan (p2_wt, p2_wt, posscan_str)
 
-
 if __name__ == "__main__":
     args = sys.argv
-    ab_name = 1
-    pdb_name = 2
-    f = 3
-
     ab_name = args[1]
     pdb_name = args[2]
-    f = args[3]
-    calculate_ddgs_from_estimator_coefficients(f, ab_name = ab_name, pdb_struct=pdb_name +'.pdb')
+    less_str = '_less_virus_Repair'
+
+    f = 'NeutSeqData_VH3-53_66_aligned_mapped_coefficients.csv'
+    #calculate_ddgs_from_estimator_coefficients(f, ab_name = ab_name, pdb_struct=pdb_name +'.pdb')
+    build_optimal_antibody_structure_mutations(ab_name,pdb_name, pdb_name+less_str, upper_bound_ddg = -0.4)
+    
