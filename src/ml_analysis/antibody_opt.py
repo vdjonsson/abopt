2517d3c1a50bc7fbcb3cca827365fc72f6df03b5
Natalie Dullerud <dullerud@usc.edu>
2020-08-13 23:41:04 -0700

    another update to antibody_opt for looser constraint

--- src/ml_analysis/antibody_opt.py
+++ src/ml_analysis/antibody_opt.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cvxpy as cp
 
-def compute_antibodies(fitness_matrix):
+def compute_antibodies(fitness_matrix, k):
     n = len(fitness_matrix[0])
+    num_unmanaged = math.floor(n*k)
     c = cp.Variable(n, boolean = True)
     incidence_matrix = np.sign(fitness_matrix).clip(min=0)
-    constraints = [cp.sum(c) >= 1]
-    objective = cp.Minimize(-1*cp.sum(cp.pos(incidence_matrix@c))-cp.matmul(cp.sum(fitness_matrix, axis=0), c) + cp.norm1(c))
+    constraints = [cp.sum(c) >= 1, cp.sum_smallest(incidence_matrix@c, num_unmanaged+1) >= 1]
+    objective = cp.Minimize(cp.norm1(c)-cp.matmul(cp.sum(fitness_matrix, axis=0), c))
     problem = cp.Problem(objective, constraints)
     result = problem.solve()
     return c.value
