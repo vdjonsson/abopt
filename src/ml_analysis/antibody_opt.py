61d4215e52ac2b2e48f332738fee8355d60027a2
Natalie Dullerud <dullerud@usc.edu>
2020-08-14 09:32:06 -0700

    antibody matrix working

--- src/ml_analysis/antibody_opt.py
+++ src/ml_analysis/antibody_opt.py
@@ -1,13 +1,25 @@
 import numpy as np
 import cvxpy as cp
+import pandas as pd
 
-def compute_antibodies(fitness_matrix, k):
-    n = len(fitness_matrix[0])
-    num_unmanaged = math.floor(n*k)
+def compute_antibodies(fitness_matrix, k, lmbda):
+    m,n = fitness_matrix.shape
+    num_unmanaged = int(m*k)
     c = cp.Variable(n, boolean = True)
     incidence_matrix = np.sign(fitness_matrix).clip(min=0)
-    constraints = [cp.sum(c) >= 1, cp.sum_smallest(incidence_matrix@c, num_unmanaged+1) >= 1]
-    objective = cp.Minimize(cp.norm1(c)-cp.matmul(cp.sum(fitness_matrix, axis=0), c))
+    constraints = [cp.sum(c) >= 1, cp.sum_smallest((incidence_matrix@c), num_unmanaged+1) >= 1]
+    objective = cp.Minimize(lmbda*cp.norm1(c)-cp.matmul(cp.sum(fitness_matrix, axis=0), c))
     problem = cp.Problem(objective, constraints)
     result = problem.solve()
+    print(c.value)
     return c.value
+
+filepath = '../data/'
+filename = 'antibody_matrix'
+k = 0.15
+lmbda = 577
+
+df = pd.read_csv(filepath+filename+'.csv', sep=',', header=0, index_col=0)
+fitness_matrix = df.values
+results = compute_antibodies(fitness_matrix, k)
+
