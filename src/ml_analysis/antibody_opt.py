300e2a13583d5ea03cc268f019ac7a3732bf5b48
Natalie Dullerud <dullerud@usc.edu>
2020-08-13 19:57:26 -0700

    changes to antibody opt problem

--- src/ml_analysis/antibody_opt.py
+++ src/ml_analysis/antibody_opt.py
@@ -5,8 +5,8 @@ def compute_antibodies(fitness_matrix):
     n = len(fitness_matrix[0])
     c = cp.Variable(n, boolean = True)
     incidence_matrix = np.sign(fitness_matrix)
-    
-    objective = cp.Minimize(-1*cp.sum(np.sign(incidence_matrix @ c)) - cp.matmul(cp.sum(fitness_matrix, axis=0), c) + cp.norm1(c))
-    problem = cp.problem(objective)
-    x = problem.solve()
-    return x.value
+    constraints = [cp.sum(c) >= 1]
+    objective = cp.Minimize(-1*cp.sum((incidence_matrix @ c)) - cp.matmul(cp.sum(fitness_matrix, axis=0), c) + cp.norm1(c))
+    problem = cp.Problem(objective, constraints)
+    result = problem.solve()
+    return c.value
