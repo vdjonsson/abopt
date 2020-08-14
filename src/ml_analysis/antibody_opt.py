7de6d1427de8d9f8ebdb4480c2c332ae4a70f8b5
Natalie Dullerud <dullerud@usc.edu>
2020-08-13 21:42:33 -0700

    now added to objective fn

--- src/ml_analysis/antibody_opt.py
+++ src/ml_analysis/antibody_opt.py
@@ -2,11 +2,11 @@ import numpy as np
 import cvxpy as cp
 
 def compute_antibodies(fitness_matrix):
-        n = len(fitness_matrix[0])
+    n = len(fitness_matrix[0])
     c = cp.Variable(n, boolean = True)
     incidence_matrix = np.sign(fitness_matrix).clip(min=0)
-    constraints = [cp.sum(c) >= 1, incidence_matrix@c >= np.ones(n)]
-    objective = cp.Minimize(-1*cp.matmul(cp.sum(fitness_matrix, axis=0), c) + cp.norm1(c))
+    constraints = [cp.sum(c) >= 1]
+    objective = cp.Minimize(cp.sum(cp.pos(incidence_matrix@c))-1*cp.matmul(cp.sum(fitness_matrix, axis=0), c) + cp.norm1(c))
     problem = cp.Problem(objective, constraints)
     result = problem.solve()
     return c.value
