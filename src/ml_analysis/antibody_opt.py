3016f6a57f25dafa1bc0a297801fb4347695d86f
Natalie Dullerud <dullerud@usc.edu>
2020-08-13 21:30:09 -0700

    correct constraint

--- src/ml_analysis/antibody_opt.py
+++ src/ml_analysis/antibody_opt.py
@@ -2,11 +2,11 @@ import numpy as np
 import cvxpy as cp
 
 def compute_antibodies(fitness_matrix):
-    n = len(fitness_matrix[0])
+        n = len(fitness_matrix[0])
     c = cp.Variable(n, boolean = True)
-    incidence_matrix = np.sign(fitness_matrix)
-    constraints = [cp.sum(c) >= 1]
-    objective = cp.Minimize(-1*cp.sum((incidence_matrix @ c)) - cp.matmul(cp.sum(fitness_matrix, axis=0), c) + cp.norm1(c))
+    incidence_matrix = np.sign(fitness_matrix).clip(min=0)
+    constraints = [cp.sum(c) >= 1, incidence_matrix@c >= np.ones(n)]
+    objective = cp.Minimize(-1*cp.matmul(cp.sum(fitness_matrix, axis=0), c) + cp.norm1(c))
     problem = cp.Problem(objective, constraints)
     result = problem.solve()
     return c.value
